40652dd4e859833150204d4a0ffe040f0ae039ac
ittat@IMac <ittat@live.com>
2021-11-07 10:48:24 +0800

    sfgsf

--- lib/main.dart
+++ lib/main.dart
@@ -1,82 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:fluro/fluro.dart';
+import 'package:mall/config/routers.dart';
+import 'package:mall/config/application.dart';
+import 'package:mall/model/user_info.dart';
+import 'package:provider/provider.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(ChangeNotifierProvider(
+    builder: (context) => UserInfoModel(),
+    child: MallApp(),
+  ));
+}
 
-class MyApp extends StatelessWidget {
+class MallApp extends StatelessWidget {
+  MallApp() {
+    final router = FluroRouter();
+    Routers.configureRoutes(router);
+    Application.router = router;
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      onGenerateRoute: Application.router.generator,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Git Hub Actions Sample App'),
-    );
-  }
-}
+        primaryColor: Colors.deepOrangeAccent,
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  int _index = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-            Text(
-              '$_index',
-              style: Theme.of(context).textTheme.display1,
-            ),
-            )
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), 
-      bottomNavigationBar:BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home), title: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.search), title: 'Search'),
-          BottomNavigationBarItem(icon: Icon(Icons.person), title: 'Profile'),
-        ],
-        currentIndex: 1,
-        onTap: (index){
-          // toast message index
-          print(index);
-          _index = index;
-
-        },
       ),
     );
   }
